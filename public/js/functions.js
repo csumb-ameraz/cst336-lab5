01fc6c54a49f8b3c447c2e1ce2f5eb42c864e990
ameraz <ameraz@csumb.edu>
2019-08-07 02:48:08 -0400

    Adding hosting info

--- public/js/functions.js
+++ public/js/functions.js
@@ -30,7 +30,8 @@ $(document).ready(function(){
          $("#favorites").html("");
          rows.forEach(function(row){
           
-           $("#favorites").append("<img class='image'src='" + row.imageURL + "' width='200' height='200'>");
+           $("#favorites").append("<img class='image'src='" + row.imageURL + "' width='200' height='200'><img class='favoriteIcon' src='img/favorite.png' width='20'>");
+           
          });
        }
     });//ajax
